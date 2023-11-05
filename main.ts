442a2935c75360a46cb21ae3774ae4dd2ba31cae
juliusfrey <149900777+juliusfrey@users.noreply.github.com>
2023-11-05 12:08:57 +0100

    alarmanlage

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         basic.setLedColor(0xff0000)
+        music.playMelody("C5 B A G F E D C ", 120)
         music.playTone(262, music.beat(BeatFraction.Whole))
         music.playTone(349, music.beat(BeatFraction.Half))
         basic.setLedColor(0x000000)
